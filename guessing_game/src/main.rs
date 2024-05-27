8f3a7b841302e678672a7a058687c9ba7d7b5de9
Bruno Kiafuka <brunokiafuka@live.com.pt>
2024-05-27 21:21:29 +0100

    guessing numbers

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,14 +1,34 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("== Guess the number == ");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
     println!("Please input your guess >> ");
+    loop {
+        let mut guess = String::new();
 
-    let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(x) => x,
+            Err(_) => continue,
+        };
 
-    println!("You guessed {guess}");
+        println!("You guessed {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Greater => println!("Too Big"),
+            Ordering::Less => println!("Too Small"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
